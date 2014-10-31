b16c5c7a775686efbd0aad3d9f508df8958deb65
Karina Simpson <ksimpson@usna.edu>
2014-10-31 16:46:47 -0400

    Tweet API get user statuses

---
+++ seetimeline.php
@@ -0,0 +1,28 @@
+<?php
+//We use already made Twitter OAuth library  https://github.com/jublonet/codebird-php
+require_once ('./bower_components/codebird-php/src/codebird.php');
+//Twitter OAuth Settings
+$CONSUMER_KEY = '...';
+$CONSUMER_SECRET = '...';
+$ACCESS_TOKEN = '.....';
+$ACCESS_TOKEN_SECRET = '...';
+//Get authenticated
+Codebird::setConsumerKey($CONSUMER_KEY, $CONSUMER_SECRET);
+$cb = Codebird::getInstance();
+$cb->setToken($ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+//retrieve posts
+$q = $_POST['q'];
+$count = $_POST['count'];
+$api = $_POST['api'];
+//https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+//https://dev.twitter.com/docs/api/1.1/get/search/tweets
+$params = array(
+'screen_name' => $q,
+'q' => $q,
+'count' => $count
+);
+//Make the REST call
+$data = (array) $cb->$api($params);
+//Output result in JSON, getting it ready for jQuery to process
+echo json_encode($data);
+?>
