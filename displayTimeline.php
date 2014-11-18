0047c1ac25677ef9e80002104dfa70022a993483
Karina Simpson <ksimpson@usna.edu>
2014-11-18 11:52:47 -0500

    Removed the require_once for codebird library. not needed.

--- displayTimeline.php
+++ displayTimeline.php
@@ -1,6 +1,4 @@
 <?php
-//We use already made Twitter OAuth library  https://github.com/jublonet/codebird-php
-require_once ('bower_components/codebird-php/src/codebird.php');
 $cache = "tweet-cache-html.txt";
 // Read cache file. Cache File will update once every 15 min.
 if (file_exists($cache)) {
